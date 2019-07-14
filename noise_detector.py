aac856829c7119625b46f3fcfea7e737362de423
Matthew D. Scholefield <matthew331199@gmail.com>
2019-07-13 19:26:16 -0700

    Drastically improve detection
    This rewrites the noise detection algorithm
    to save a dictionary of the temporal frequency
    distributions across each band. As a consequence,
    many of the previous variables no longer make
    sense

--- noise_detector.py
+++ noise_detector.py
@@ -4,11 +4,8 @@ Detect noise changes in the environment
 :on_noise str
     The command to run when noise is detected
 
-:-v --trigger-volume float 3.5
-    The number of standard deviations from the average volume to consider a noise change
-
-:-a --adjustment-speed float 0.01
-    The speed at which the ambient noise level changes. Between 0 and 1
+:-v --trigger-volume float 0.9
+    The average abnormality exceeded that is considered an activation. Between 0.0 and 1.0
 
 :-d --delay float 1.0
     The delay in seconds between subsequent activations
@@ -16,16 +13,17 @@ Detect noise changes in the environment
 :-b --bands str -
     A range/list of bands to listen for
 """
+from collections import deque
+
+import json
+import numpy as np
 import os
 import re
-from functools import partial
-from subprocess import Popen
-
-import numpy as np
 from logzero import logger
 from prettyparse import create_parser
 from pylisten import FeatureListener
-from sonopy import mel_spec
+from sonopy import power_spec, safe_log
+from subprocess import Popen
 
 
 def parse_bands(s, m):
@@ -38,63 +36,108 @@ def parse_bands(s, m):
     return [int(s)]
 
 
+def get_bands(args, parser, num_filt):
+    if args.bands:
+        try:
+            return parse_bands(args.bands, num_filt)
+        except ValueError:
+            parser.error('Invalid brands expression')
+            return
+    else:
+        return list(range(num_filt))
+
+
+def averager_gen(adjustment_speed=0.01):
+    averages = feature = yield
+    av_diff = np.zeros_like(feature)
+
+    while True:
+        diff = feature - averages
+        av_diff += adjustment_speed * (diff ** 2 - av_diff)
+        averages += adjustment_speed * diff
+        av_freq_stds = np.sqrt(av_diff)
+        freq_stds = diff / np.clip(av_freq_stds, np.finfo(float).eps, None)
+        feature = yield freq_stds
+
+
+class BandCounter:
+    def __init__(self, memory):
+        self.memory = memory
+        self.coords = deque()
+        self.counts = {}
+
+    def update(self, coord: np.ndarray) -> float:
+        """Returns the abnormality of this band as a float from 0.0 to 1.0"""
+        k = self._key(coord)
+        self.coords.append(k)
+        existing_count = self.counts.get(k, 0)
+        self.counts[k] = existing_count + 1
+        if len(self.coords) > self.memory:
+            ok = self.coords.popleft()
+            nv = self.counts[ok] - 1
+            if nv:
+                self.counts[ok] = nv
+            else:
+                del self.counts[ok]
+        return 1.0 - existing_count / self.memory
+
+    def _key(self, x):
+        return (x / self._calc_space()).astype(int).data.tobytes()
+
+    def _calc_space(self):
+        return 1.5  # TODO: Periodically regenerate this and recalculate counts
+
+
+class SpecCounter:
+    def __init__(self, memory, bands: list):
+        self.bands = {band: BandCounter(memory) for band in bands}
+
+    def update(self, frames):
+        """Returns a dict mapping bands to the abnormality ratio"""
+        coords = frames.T
+        return {band: counter.update(coords[band]) for band, counter in self.bands.items()}
+
+
 def main():
     parser = create_parser(__doc__)
 
     sample_rate = 16000
-    stride = 500
-    width = 2
-    num_filt = 20
-    averages = None
-    av_diff = None
+    stride = 400
+    width = 4
+    delay = 6.0
+    num_bands = 257
     chunk_seconds = stride / sample_rate
-    delay = 2.0
+    averager = averager_gen()
+    next(averager)
 
     args = parser.parse_args()
-    try:
-        bands = parse_bands(args.bands, num_filt)
-        erase_bands = np.array([i not in bands for i in range(num_filt)])
-    except ValueError:
-        parser.error('Invalid brands expression')
-        return
-    print(bands)
+    bands = get_bands(args, parser, num_bands)
 
-    processor = partial(
-        mel_spec, sample_rate=sample_rate, window_stride=(2 * stride, stride),
-        fft_size=512, num_filt=num_filt
-    )
+    def processor(audio):
+        log_spec = safe_log(power_spec(
+            audio, window_stride=(2 * stride, stride),
+            fft_size=512
+        ))
+        return np.array([averager.send(i) for i in log_spec] or log_spec)
+
+    counts = SpecCounter(200, bands)
     for features in FeatureListener(processor, stride, width, dict(rate=sample_rate)):
-        feature = features[0] + features[1]  # Average across two frames
+        abnormalities = counts.update(features)
+        av_abnormality = sum(abnormalities.values()) / len(abnormalities)
 
-        if averages is None:
-            logger.info('Calculating ambient noise...')
-            averages = feature
-            av_diff = np.zeros_like(feature)
+        if delay > 0:
+            delay -= chunk_seconds
+            if delay <= 0:
+                logger.info('Listening...')
         else:
-            diff = feature - averages
-            av_diff += args.adjustment_speed * (diff ** 2 - av_diff)
-            averages += args.adjustment_speed * diff
-
-            if delay > 0:
-                delay -= chunk_seconds
-                if delay <= 0:
-                    logger.info('Listening...')
-            else:
-                av_freq_stds = np.sqrt(av_diff)
-                freq_stds = diff / av_freq_stds
-                max_band = np.argmax(np.where(erase_bands, float('-inf'), freq_stds))
-                max_freq_std = freq_stds[max_band]
-                if max_freq_std > args.trigger_volume and max_band in bands:
-                    logger.info('Activation of {:.2f} on band {}!'.format(max_freq_std, max_band))
-                    Popen(args.on_noise, shell=True, env=dict(
-                        os.environ,
-                        VOLUME='{:.2f}'.format(max_freq_std),
-                        BAND=str(max_band),
-                        BANDS=' '.join(map(str, freq_stds)),
-                        AMBIENT_VOLUME=str(averages[max_band]),
-                        AMBIENT_STD=str(av_diff[max_band])
-                    ))
-                    delay = args.delay
+            if av_abnormality >= args.trigger_volume:
+                logger.info('Activation of {:.2f}'.format(av_abnormality))
+                Popen(args.on_noise, shell=True, env=dict(
+                    os.environ,
+                    VOLUME='{:.2f}'.format(av_abnormality),
+                    BANDS=json.dumps(abnormalities, sort_keys=True)
+                ))
+                delay = args.delay
 
 
 if __name__ == '__main__':
